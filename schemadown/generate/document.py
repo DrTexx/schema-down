5cf4c49689dfc6796dbdb2e6c7179d8d12dea870
DrTexx <23412100+DrTexx@users.noreply.github.com>
2020-04-01 20:43:00 +1100

    (wip) working on schema dict recursion

--- schemadown/generate/document.py
+++ schemadown/generate/document.py
@@ -7,6 +7,41 @@ from mdutils.mdutils import MdUtils
 from ..types import JSONSchema, DocList, MarkdownDocument
 
 
+def resolve_ref(v) -> None:
+    print("(wip) RESOLVING:", v)
+
+
+def resolve_all(entry, parent) -> dict:
+    # for every item in the entry
+    for k, v in entry.items():
+        # if the items value is a dict
+        if isinstance(v, dict):
+            # resolve the dict
+            resolve_all(v, entry)
+        # if the items value isn't a dict
+        else:
+            # if items value is a ref
+            if k == "$ref":
+                # resolve ref
+                resolve_ref(v)
+            # if items value is NOT a ref
+            else:
+                # print items value
+                print(f"{k} : {v}")
+    return entry
+
+
+# TODO: removable, reference code for dict recursion
+def myprint(d):
+    for k, v in d.items():
+        if isinstance(v, dict):
+            myprint(v)
+        else:
+            if k == "$ref":
+                myprint()
+            print("{0} : {1}".format(k, v))
+
+
 class SchemaDownDocument:
     """Standard format for schema documents despite output format. Has methods to output in specific formats."""
 
@@ -29,7 +64,7 @@ class SchemaDownDocument:
         """Return document as markdown."""
         # TODO: example, needs to be dynamic
 
-        mdFile = MdUtils(file_name="Example", title="Schema")
+        mdFile = MdUtils(file_name="file1.md", title="Schema")
         mdFile.new_header(level=1, title=self.title)
 
         # document meta headings
@@ -38,9 +73,16 @@ class SchemaDownDocument:
         documentMeta.extend(["Schema Id", self.id])
         documentMeta.extend(["Title", self.title])
 
-        # TODO: generate code snippet sections like those in the gantree docs as of 2020/03/29
-
         mdFile.new_table(columns=2, rows=3, text=documentMeta)
+
+        # TODO: generate code snippet sections like those in the gantree docs as of 2020/03/29
+        mdFile.new_paragraph('```jsonc\n{\n  "example": "text"\n}\n```')
+
+        props = self.schema["properties"]
+
+        resolved = resolve_all(props, None)
+
+        print("resolved:", resolved)
 
         # style is set 'atx' format by default.
 
@@ -52,11 +94,10 @@ class SchemaDownDocument:
         #     columns=2, rows=6, text=list_of_strings, text_align="center"
         # )
 
-        print(dir(mdFile))
+        filename = mdFile.file_name
+        content = mdFile.file_data_text
 
-        stuff = mdFile.file_data_text
-
-        print(stuff)
+        return {"filename": filename, "content": content}
 
         # mdFile.create_md_file()
         #
